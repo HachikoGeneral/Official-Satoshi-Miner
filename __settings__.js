cd12e8c92d027179172ad6cae6f5102002bd71ac
HachikoGeneral <93988533+HachikoGeneral@users.noreply.github.com>
2025-02-08 21:18:48 +0000

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2165425.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 213430152, 213430157, 213430230, 213430194, 213430165, 213430161, 213430145, 213430106, 213430212, 213430159, 213430204, 213430181, 213430195, 213430213, 213430192, 213430203, 213430180, 213430113, 213430173, 213430149, 213430160, 213430168, 213430158, 213430237, 213430245, 213430162, 213430151, 213430143, 213430177, 213430156, 213430148, 213430171, 213430172, 213430166, 213430164 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
